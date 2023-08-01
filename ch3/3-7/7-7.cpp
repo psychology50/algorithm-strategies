029e26a2834c324f9a87eb8d88a00d26400071a0
JaeSeo Yang <qud1251@gmail.com>
2023-08-01 20:23:38 +0900

    example

--- ch3/3-7/7-7.cpp
+++ ch3/3-7/7-7.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-// 코드 7.8 울타리 잘라내기 문제를 해결하는 O(n^2) 알고리즘
+// 코드 7.7 울타리 잘라내기 문제를 해결하는 O(n^2) 알고리즘
 
 // 판자의 높이를 담은 배열 h[]가 주어질 때 사각형의 최대 너비를 반환한다
 int bruteForce(const vector<int>& h) {
