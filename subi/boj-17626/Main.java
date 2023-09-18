cfd64bd170817904a4b7b4a9fd53c5efd15f94f5
Subi Hwang <ghkdtnql@gmail.com>
2023-09-18 13:24:50 +0900

    commit

--- subi/boj-17626/Main.java
+++ subi/boj-17626/Main.java
@@ -1,24 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
-    int[] dp = new int[n + 1];
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    dp[0] = 0;
-    dp[1] = 1;
-    for(int i = 2; i <= n; i++) {
-      dp[i] = dp[i - 1];
-      for(int j = 1; j * j <= i; j++) {
-        dp[i] = Math.min(dp[i], dp[i - (j * j)]);
-      }
-      dp[i]++;
-    }
-    System.out.println(dp[n]);
+    int input = Integer.parseInt(br.readLine());
+
+    int result = findSquareNumber(input);
+    bw.write(String.valueOf(result));
+    bw.close();
   }
 
+  static int findSquareNumber(int input) {
+    int count = 0;
+    int MAX = 0;
+    int[] cache = new int[224];
+
+    for (int i = 0; i < 224; i++) {
+      cache[i] = (int) Math.pow(i, i);
+    }
+
+    while (input > 1) {
+      int maxSquare = -1;
+
+      for (int i = 1; i < cache.length; i++) {
+        if (cache[i] <= input && cache[i] > maxSquare) {
+          maxSquare = cache[i];
+          System.out.println(maxSquare);
+        }
+      }
+
+      if (maxSquare > 0) {
+        input -= maxSquare;
+        count++;
+      } else {
+        break; // 더 이상 input을 만족하는 제곱수를 찾을 수 없으면 종료
+      }
+    }
+
+    return count;
+  }
 }
