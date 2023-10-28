669324bf45cc837f8171d899aac14d99ffb44bf0
Subi Hwang <ghkdtnql@gmail.com>
2023-10-28 20:46:52 +0900

    11170번 사용자입력 구현

--- subi/boj-11170/src/Main.java
+++ subi/boj-11170/src/Main.java
@@ -1,5 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+  static int min,max;
+
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    int CASE = Integer.parseInt(br.readLine());
+
+    for(int i=0;i<CASE;i++){
+      st = new StringTokenizer(br.readLine()," ");
+      min = Integer.parseInt(st.nextToken());
+      max = Integer.parseInt(st.nextToken());
+    }
+
+
+    br.close();
   }
 }
