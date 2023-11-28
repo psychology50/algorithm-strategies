d4ad6ad2833f90167dd579a29e99bfc3615c642d
Subi Hwang <ghkdtnql@gmail.com>
2023-11-28 19:44:27 +0900

    반만 맞아서 일단 커밋

--- subi/softeer-level2-metal/src/Main.java
+++ subi/softeer-level2-metal/src/Main.java
@@ -1,9 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class Main {
+
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,34 +15,38 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		int W = Integer.parseInt(st.nextToken());
 		int N = Integer.parseInt(st.nextToken());
-
-		List<AbstractMap.SimpleEntry<Integer, Integer>> list = new ArrayList<>();
+		//두 개의 값이 들어갈 수 있다.
+		ArrayList<int[]> arr = new ArrayList<>();
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int metalW = Integer.parseInt(st.nextToken());
 			int P = Integer.parseInt(st.nextToken());
-			list.add(new AbstractMap.SimpleEntry<>(P, metalW));
-
-			list.sort(Map.Entry.<Integer, Integer>comparingByKey().reversed());
-
-			int result = 0;
-			while (W >= 0) {
-				for (AbstractMap.SimpleEntry<Integer, Integer> entry : list) {
-					if (W <= entry.getKey()) {
-						result = W * entry.getValue();
-					} else {
-						int num = entry.getKey() * entry.getValue();
-						W -= num;
-					}
-				}
-			}
-
-			System.out.println(result);
-
-
+			arr.add(new int[] {metalW, P});
 		}
 
-	}
+		//2번째 요소를 기준으로 내림차순
+		Collections.sort(arr, Comparator.comparingInt((int[] a) -> a[1]).reversed());
 
+		int result = 0;
+		int sum = 0;
+		int count = 0;
+
+		for (int[] array : arr) {
+			//array[0] = 금속 무게, array[1] = 금속 가치
+			while (sum <= W) {
+				sum += array[0];
+				result += array[0] * array[1];
+				count++;
+				if (sum < W) {
+					int value = arr.get(count)[1];
+					int left = W - sum;
+					result = left * value;
+				}
+			}
+		}
+
+		System.out.println(result);
+
+	}
 }
