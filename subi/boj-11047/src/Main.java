2bb5cb18cec98107bc9c7062fee3b3878f578f77
Subi Hwang <ghkdtnql@gmail.com>
2023-12-03 19:48:53 +0900

    11047번 커밋

--- subi/boj-11047/src/Main.java
+++ subi/boj-11047/src/Main.java
@@ -1,37 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
+
+	static int arr[];
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		st = new StringTokenizer(br.readLine());
+
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 
-		Integer[] arr = new Integer[n];
+		arr = new int[n];
 
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < arr.length; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 
-		Arrays.sort(arr, Collections.reverseOrder());
-
 		int count = 0;
 
-		for (int a : arr) { //50000,10000,5000,1000 //k = 4200
-			if (k >= a) {
-				count = count + k / a;
-				k = k % a;
+		for (int i = arr.length - 1; i >= 0; i--) {
+			if (arr[i] <= k) {
+				count += (k / arr[i]);
+				k = k % arr[i];
 			}
 		}
 
 		System.out.println(count);
+
 
 	}
 }
