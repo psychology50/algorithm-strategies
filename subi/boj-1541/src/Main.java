591dfe72096e5cd6860554f45a470082c188ebeb
Subi Hwang <ghkdtnql@gmail.com>
2023-12-03 22:01:13 +0900

    1541번 커밋

--- subi/boj-1541/src/Main.java
+++ subi/boj-1541/src/Main.java
@@ -2,31 +2,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
 public class Main {
+
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		String[] strArr = str.split("-");
+		String s[] = br.readLine().split("-");
 
-		int answer = 0;
-		for (String s : strArr) {
-			if (s == strArr[0]) {
-				answer += findSum(s);
+		int result = 0;
+
+		for (int i = 0; i < s.length; i++) {
+			if (i == 0) {
+				result = findSum(s[i]);
 			} else {
-				answer -= findSum(s);
+				result -= findSum(s[i]);
 			}
 		}
-		System.out.println(answer);
+		System.out.println(result);
 	}
 
 	private static int findSum(String s) {
-		String[] num = s.split("\\+");
+
 		int sum = 0;
-		for (String n : num) {
-			sum += Integer.parseInt(n);
+		String ss[] = s.split("\\+");
+		for (int i = 0; i < ss.length; i++) {
+			sum += Integer.parseInt(ss[i]);
 		}
 		return sum;
 	}
+
 
 }
