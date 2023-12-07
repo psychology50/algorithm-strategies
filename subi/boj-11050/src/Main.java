a4bd7bf45051400ddff27dc09bda62bcefed45f4
Subi Hwang <ghkdtnql@gmail.com>
2023-12-07 20:06:38 +0900

    11050번 커밋

--- subi/boj-11050/src/Main.java
+++ subi/boj-11050/src/Main.java
@@ -1,32 +1,31 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
+
 
 public class Main {
+
+	static int cache[][];
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int r = Integer.parseInt(st.nextToken());
-
-		int Combination[][] = new int[n + 1][n + 1];
-
-		for (int i = 0; i <= n; i++) {
-			Combination[i][0] = 1; //아무것도 안 뽑는 경우 = 1가지
-			Combination[i][i] = 1;
-			Combination[i][1] = i;
-		}
-
-		for (int i = 2; i <= n; i++) {
-			for (int j = 1; j < i; j++) { // 고르는 수의 개수가 전체 개수를 넘을 수 없다.
-				Combination[i][j] = Combination[i - 1][j] + Combination[i - 1][j - 1];
-			}
-		}
-
-		System.out.println(Combination[n][r]);
-
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		cache = new int[n + 1][k + 1];
+		int result = Combination(n, k);
+		System.out.println(result);
 	}
+
+	private static int Combination(int n, int k) {
+		if (n == k || k == 0) {
+			return cache[n][k] = 1;
+		}
+
+		if (cache[n][k] != 0) {
+			return cache[n][k];
+		}
+
+		return cache[n][k] = Combination(n - 1, k - 1) + Combination(n - 1, k);
+	}
+
+
 }
