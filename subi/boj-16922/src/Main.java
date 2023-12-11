62b9973b1f1d3487df5f71ea1379f11fb71e0380
Subi Hwang <ghkdtnql@gmail.com>
2023-12-11 17:14:21 +0900

    16922번 커밋

--- subi/boj-16922/src/Main.java
+++ subi/boj-16922/src/Main.java
@@ -1,31 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	static int N;
-	static int num[];
-	static int sum[];
-	static int res;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		num = new int[] {1, 5, 10, 50};
-		sum = new int[10000];
-		pick(0, 0, 0);
-		System.out.println(res);
+	static int[] num = {1, 5, 10, 50};
+	static boolean[] dp = new boolean[1001];
+	static int result = 0, N = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		pick(N, 0, 0);
+		System.out.println(result);
 	}
 
-	static void pick(int cnt, int idx, int s) {
-		if (cnt == N) {
-			if (sum[s] == 0) {
-				sum[s] = 1;
-				res++;
+	private static void pick(int n, int index, int sum) {
+		if (n == 0) {
+			if (dp[sum] == false) {
+				result++;
+				dp[sum] = true;
 			}
 			return;
 		}
 
-		for (int i = idx; i < 4; i++) {
-			pick(cnt + 1, i, s + num[i]);
+		for (int i = index; i < 4; i++) {
+			pick(n - 1, i, sum + num[i]);
 		}
+
 	}
+
+
 }
