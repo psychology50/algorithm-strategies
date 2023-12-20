f05fa651617251fdeebde1fefa2c6805d0d31948
Subi Hwang <ghkdtnql@gmail.com>
2023-12-20 15:12:02 +0900

    1463번 커밋

---
+++ subi/boj-1463/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	static int[] dp;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		dp = new int[n + 1];
+
+		//0일 때는 고려하지 않아도 된다.
+		dp[1] = 0;
+
+		for (int i = 2; i < n + 1; i++) {
+			dp[i] = dp[i - 1] + 1;
+			if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+			}
+			if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+			}
+		}
+		System.out.println(dp[n]);
+	}
+}
